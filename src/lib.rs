AlessandroPerez/pqdr-quic

commit dd06ca37e3ba519b5a2640936aac72899a4fe1b1

bundle ACK with PTO probes

src/lib.rs
@@ -2085,7 +2085,10 @@ impl Connection {
         let mut payload_len = 0;
 
         // Create ACK frame.
-        if self.pkt_num_spaces[epoch].ack_elicited && !is_closing {
+        if (self.pkt_num_spaces[epoch].ack_elicited ||
+            self.recovery.loss_probes[epoch] > 0) &&
+            !is_closing
+        {
             let ack_delay =
                 self.pkt_num_spaces[epoch].largest_rx_pkt_time.elapsed();
 
@@ -6989,8 +6992,13 @@ mod tests {
         let frames =
             testing::decode_pkt(&mut pipe.server, &mut buf, len).unwrap();
 
+        let mut iter = frames.iter();
+
+        // Skip ACK frame.
+        iter.next();
+
         assert_eq!(
-            frames.iter().next(),
+            iter.next(),
             Some(&frame::Frame::Stream {
                 stream_id: 4,
                 data: stream::RangeBuf::from(b"b", 0, false),

commit 9c9bcd0961203340ff7f32ac9b583b840866c542

don't try to create ACK frames if there is nothing to ack
This is a regression introduced by dd06ca37e3ba519b5a2640936aac72899a4fe1b1.

We need to check that there are outstanding packets that need to be
acked before trying to create an ACK frame, otherwise a panic is
triggered when the frame is serialized.

src/lib.rs
@@ -2085,8 +2085,9 @@ impl Connection {
         let mut payload_len = 0;
 
         // Create ACK frame.
-        if (self.pkt_num_spaces[epoch].ack_elicited ||
-            self.recovery.loss_probes[epoch] > 0) &&
+        if self.pkt_num_spaces[epoch].recv_pkt_need_ack.len() > 0 &&
+            (self.pkt_num_spaces[epoch].ack_elicited ||
+                self.recovery.loss_probes[epoch] > 0) &&
             !is_closing
         {
             let ack_delay =